f0016e689287bf513fe2fd6d424a1b7968d8f2d0
Mikołaj Radek <radek_mikolaj@o2.pl>
2022-05-28 01:10:37 +0200

    popup and counter

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,34 @@
 const button = document.querySelector('button')
-const hide = document.querySelector('hide')
+const popup = document.querySelector('.hide_popup')
+const textCnt = document.querySelector('.counter_text')
+const addButtonCounter = document.querySelector('.counter_hide')
+let counter =  0
 
-button.addEventListener("click", buttons)
 
-function buttons() {
-    hide.classList.add('')
+button.addEventListener("click", buttonRemove)
+button.addEventListener("click", counterAdd)
+popup.addEventListener("click", buttonAdd)
+addButtonCounter.addEventListener("click", removeCounter)
+
+function buttonRemove() {
+    popup.classList.remove('hide_popup')
 }
+
+function buttonAdd() {
+    popup.classList.add('hide_popup')
+}
+function removeCounter(){
+    counter = ''
+    addButtonCounter.classList.add('counter_hide')
+}
+
+function counterAdd() {
+    counter++
+    let text = textCnt.innerHTML = `You have clicked ${counter} times to related button.`
+    if (counter >= 5){
+        addButtonCounter.classList.remove('counter_hide')
+    }
+}
+
+
+
