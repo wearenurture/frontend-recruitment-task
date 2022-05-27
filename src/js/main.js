f2290ee4267269af1f16bfc22b86733c7cd31c85
Mikołaj Radek <radek_mikolaj@o2.pl>
2022-05-27 13:10:13 +0200

    popup-static

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('button')
+const hide = document.querySelector('hide')
+
+button.addEventListener("click", buttons)
+
+function buttons() {
+    hide.classList.add('')
+}
