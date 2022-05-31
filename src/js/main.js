5526567c76c17ae754aef4f588196f24496afdfc
Katarzyna Gorgolewska <kasia771771@gmail.com>
2022-05-31 15:26:12 +0200

    task03-saving-to-localStorage

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,29 @@
 const button = document.getElementById("button");
 const count = document.getElementById("count");
 const popup = document.getElementById("popup");
-let counter = 0;
+let counter = 1;
 
+function clickCounter() {
+    if (localStorage.clickcount) {
+      localStorage.clickcount = Number(localStorage.clickcount)+1;
+    } else {
+      localStorage.clickcount = 1;
+    }
+    count.innerText = ` ${localStorage.clickcount} times`;  }
 
+const handleClick = (e) => {
+  e.preventDefault();
+  popup.classList.add("popup");
+    clickCounter();
 
-const handleClick = () => {
-    counter++;
-    popup.classList.add("popup")
-    count.innerText = `${counter} times`;
-}
+};
 
 const handleClose = (e) => {
- if(e.target.closest(".alert")){
+  if (e.target.closest(".alert")) {
     return;
- }
-    popup.classList.remove("popup")
-}
+  }
+  popup.classList.remove("popup");
+};
 
-button.addEventListener("click",handleClick);
-popup.addEventListener("click",handleClose);
-
+button.addEventListener("click", handleClick);
+popup.addEventListener("click", handleClose);
