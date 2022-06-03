184412352a4b284395f8ccd40b5c74084543e81d
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-06-03 13:46:33 +0100

    added working counter

--- src/js/main.js
+++ src/js/main.js
@@ -4,26 +4,41 @@ const state = {
   counter: 0,
 };
 
-const addCounterOnClick = () => {
-  state.counter++;
-};
-
 // view
 
 class imageView {
   showModalButton = document.querySelector(".main__button");
   modal = document.querySelector(".modal");
-  constructor() {
+  overlay = document.querySelector(".overlay");
+  modalInfo = document.querySelector(".modal__info");
+  counter;
+  constructor(counter) {
+    this.counter = counter;
     this.showModal();
+    this.hideModal();
   }
 
   showModal() {
     this.showModalButton.addEventListener("click", () => {
-      this.modal.classList.toggle("hidden");
+      this.counter++;
+      this.toggleModal();
+      console.log(this.modalInfo);
+      this.modalInfo.textContent = `You have clicked ${this.counter} times to related button`;
     });
+  }
+
+  hideModal() {
+    this.overlay.addEventListener("click", () => {
+      this.toggleModal();
+    });
+  }
+
+  toggleModal() {
+    this.overlay.classList.toggle("hidden");
+    this.modal.classList.toggle("hidden");
   }
 }
 
-const imageViewInstance = new imageView();
+const imageViewInstance = new imageView(state.counter);
 
 // controller
