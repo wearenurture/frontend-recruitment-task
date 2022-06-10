391ec911308b97422ba1fd1dd134d59599b9d87b
Piotr09 <piotr.palasz01@gmail.com>
2022-06-10 13:04:11 +0200

    feat: add html structure and styles

--- gulpfile.js
+++ gulpfile.js
@@ -7,48 +7,48 @@ const uglify = require('gulp-uglify');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
-var replace = require('gulp-replace');
+const replace = require('gulp-replace');
 
 // File paths
-const files = { 
+const files = {
     scssPath: 'src/scss/**/*.scss',
-    jsPath: 'src/js/**/*.js'
+    jsPath: 'src/js/**/*.js',
 };
 
-function scssTask(){    
-    return src(files.scssPath)
+function scssTask(done) {
+    src(files.scssPath)
         .pipe(sourcemaps.init()) // initialize sourcemaps first
-        .pipe(sass([])) // compile SCSS to CSS
-        .pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss([autoprefixer(), cssnano()])) // PostCSS plugins
         .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
-        .pipe(dest('dist')
-    ); // put final CSS in dist folder
-}
+        .pipe(dest('dist'))
+    done();
+};
 
-function jsTask(){
+function jsTask() {
     return src([
         files.jsPath
-        ])
+    ])
         .pipe(concat('all.js'))
         .pipe(uglify())
         .pipe(dest('dist')
-    );
+        );
 }
 
 var cbString = new Date().getTime();
-function cacheBustTask(){
+function cacheBustTask() {
     return src(['index.html'])
         .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
         .pipe(dest('.'));
 }
 
-function watchTask(){
-    watch([files.scssPath, files.jsPath], 
-        parallel(scssTask, jsTask));    
+function watchTask() {
+    watch([files.scssPath, files.jsPath],
+        parallel(scssTask, jsTask));
 }
 
 exports.default = series(
-    parallel(scssTask, jsTask), 
+    parallel(scssTask, jsTask),
     cacheBustTask,
     watchTask
 );
