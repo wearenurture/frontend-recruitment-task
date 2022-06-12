555becd837b6512f9e195323c490d303539e4581
Artur Haber <artstudiobytom@gmail.com>
2022-06-12 21:56:32 +0200

    finish changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,63 @@
-let countResult =0;
-const button = document.getElementById("sectionButton");
-button.addEventListener('click', Render)
-function Render(){
-countResult+=1;
-let html = `<div class="popupBackground"><div class="popupBackground__popup"><div class="popupBackground__popup--exit" id="exit"></div><h2 class="popupBackground__popup--title">Alert!</h2><p class="popupBackground__popup--text">You have clicked <span class="popupBackground__popup--boldText">${countResult} times</span> to related button.</p></div></div>`;
-document.getElementById("popup").innerHTML = html;
-const exit = document.getElementById("exit");
-exit.addEventListener('click',()=>{document.getElementById("popup").innerHTML ='';})
+if(document.readyState !== 'loading') {
+    TrackingClickOpenButton();
 }
+else {
+    document.addEventListener('DOMContentLoaded', function () {
+        TrackingClickOpenButton();
+    });
+}
+
+function TrackingClickOpenButton(){
+    const button = document.getElementById("sectionButton");
+    button.addEventListener('click', Render);
+}
+
+
+function Render(){
+    Count();
+
+    let template = `<div class="backgroundPopup" id="backgroundPopup"></div><div class="popupModal"><div class="popupModal__popup"><button class="popupModal__popup--exit" id="exit"></button><h2 class="popupModal__popup--title">Alert!</h2><p class="popupModal__popup--text">You have clicked <span class="popupModal__popup--boldText">${sessionStorage.clickcount} times</span> to related button.</p>`;
+    if (sessionStorage.clickcount>=5) {
+        template+= '<button class="popupModal__popup--reset" id="reset">Reset</button>';
+    }
+    template+='</div></div>';
+    document.getElementById("popup").innerHTML = template;
+
+    const reset = document.getElementById("reset");
+    if (reset!=null) {
+        reset.addEventListener('click', Reset);
+    }
+
+    Exit();
+}
+
+function Exit(){
+    const exit = document.getElementById("exit");
+    const background = document.getElementById("backgroundPopup");
+    if (exit!=null && background!=null) {
+        exit.addEventListener('click', CloseTemplate);
+        background.addEventListener('click', CloseTemplate);
+    }
+}
+
+function CloseTemplate() {
+        document.getElementById("popup").innerHTML ='';
+}
+
+function Count() {
+    if (sessionStorage.clickcount) {
+        sessionStorage.clickcount = Number(sessionStorage.clickcount) + 1;
+    } else {
+        sessionStorage.clickcount = 0;
+    }
+}
+function Reset(){
+    sessionStorage.clickcount = 0;
+    CloseTemplate();
+}
+
+
+
+
+
+
