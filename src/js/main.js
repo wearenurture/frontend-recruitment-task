581ec43de7ab7e2ece915a4834504cb0eba1446d
Jakub Barszcz <jakub.barszcz33@gmail.com>
2022-06-14 20:19:43 +0200

    add reset button

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,47 @@
  const clickBtn = document.querySelector('.section__aside-btn');
  const clickCounter = document.querySelector('#click');
- const  popupInfo = document.querySelector('.popup');
- const  popupShadow = document.querySelector('.popup__shadow');
- const  popupCloseBtn = document.querySelector('.popup__close-btn');
- 
- let countBtn = 0;
+ const popupInfo = document.querySelector('.popup');
+ const popupShadow = document.querySelector('.popup__shadow');
+ const popupCloseBtn = document.querySelector('.popup__close-btn');
+ const resetBtn = document.querySelector('.popup__btn')
+ let countBtn = 1;
 
- const showPopup = () => {
+ function counterClick() {
+    if (localStorage.count) {
+      localStorage.count = Number(localStorage.count) + 1;
+    } else {
+      localStorage.count = 1;
+    }
+    clickCounter.textContent = ` ${localStorage.count} times`;
+  } 
+  function borderCount () {
+    if (localStorage.count >= 5) {
+      resetBtn.classList.add("active");
+    } else {
+      resetBtn.classList.remove("active");
+    }
+  };
+ const showPopup = (e) => {
+    e.preventDefault();
     popupShadow.classList.add('active');
-    popupInfo.classList.add('active')
-    countBtn++;
-    clickCounter.textContent = countBtn + " times";
+    popupInfo.classList.add('active');
+    counterClick();
+    borderCount();
 }
-
-const closePopup = () => {
+const closePopup = () =>{
     popupShadow.classList.remove('active');
     popupInfo.classList.remove('active');
-    countBtn = 0;
-    clickCounter.textContent = 0;
+    clickCounter.textContent = count;
+}
+const resetPopup = () => {
+    popupShadow.classList.remove('active');
+    popupInfo.classList.remove('active');
+    localStorage.clear();
 }
 
-clickBtn.addEventListener('click',showPopup);
-popupCloseBtn.addEventListener('click',closePopup);
+
+clickBtn.addEventListener('click', showPopup);
+popupCloseBtn.addEventListener('click', closePopup);
+popupShadow.addEventListener('click', closePopup);
+resetBtn.addEventListener('click', resetPopup);
+
